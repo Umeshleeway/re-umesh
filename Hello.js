49ced828993c15204b75db387bd73b8f8faccae6
umesh <umesh@leewayhertz.com>
2022-03-30 11:18:37 +0530

    example of merge requst

--- Hello.js
+++ Hello.js
@@ -1,4 +1,10 @@
 console.log("changes gor push");
+<<<<<<< HEAD
 console.log("add another branch");
 console.log("add new one");
 console.log("Raaja");
+=======
+console.log("add new  branchnew features ");
+console.log("add new one new features");
+console.log("Raaja");
+>>>>>>> 38cdb407d242d5b1dca38e2a9aba0f7a55cf250b
